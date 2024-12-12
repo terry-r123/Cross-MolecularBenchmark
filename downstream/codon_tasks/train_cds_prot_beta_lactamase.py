a9bb44a25e3e6a49a4348f9a4ee971c0cf5ffc53
asadiry <mikudiary@126.com>
2024-12-12 12:34:13 +0800

    add cds related

--- downstream/codon_tasks/train_cds_prot_beta_lactamase.py
+++ downstream/codon_tasks/train_cds_prot_beta_lactamase.py
@@ -28,34 +28,25 @@ sys.path.append(parent_dir)
 from model.dnabert2.bert_layers import BertForSequenceClassification as DNABERT2ForClassification
 from model.ntv2.modeling_esm import EsmForSequenceClassification as NTv2ForSequenceClassification
 from model.rnafm.modeling_rnafm import RnaFmForSequenceClassification
-
 from model.esm.modeling_esm import EsmForSequenceClassification
-# from tokenizer.tokenization_opensource import OpenRnaLMTokenizer
-
-# from model.lucagplm.v0_2.lucaone_gplm import LucaGPLM as LucaGPLMV0_2
-# from model.lucagplm.v0_2.lucaone_gplm_config import LucaGPLMConfig as LucaGPLMConfigV0_2
-# from model.lucagplm.v0_2.alphabet import Alphabet as AlphabetV0_2
-# from model.lucagplm.v2_0.lucaone_gplm import LucaGPLM as LucaGPLMV2_0
-# from model.lucagplm.v2_0.lucaone_gplm_config import LucaOneConfig
-# from model.lucagplm.v2_0.alphabet import Alphabet as AlphabetV2_0
-# from model.lucagplm.v2_0.modeling_gplm import LucaOneForSequenceClassification
+from model.lucagplm.v2_0.modeling_gplm import LucaOneForSequenceClassification
 
 # from peft import LoraConfig, get_peft_model
-# change for LucaOne
 
 early_stopping = EarlyStoppingCallback(early_stopping_patience=20)
 
 @dataclass
 class ModelArguments:
+    tokenizer_name_or_path: Optional[str] = field(default="facebook/esm1b_t33_650M_UR50S")
     model_name_or_path: Optional[str] = field(default="facebook/esm1b_t33_650M_UR50S")
+    use_alibi: bool = field(default=True, metadata={"help": "whether to use alibi"})
+    
     # use_lora: bool = field(default=False, metadata={"help": "whether to use LoRA"})
-    # use_alibi: bool = field(default=True, metadata={"help": "whether to use alibi"})
     # use_features: bool = field(default=True, metadata={"help": "whether to use alibi"})
     # lora_r: int = field(default=8, metadata={"help": "hidden dimension for LoRA"}) # 8
     # lora_alpha: int = field(default=32, metadata={"help": "alpha for LoRA"})
     # lora_dropout: float = field(default=0.05, metadata={"help": "dropout rate for LoRA"})
     # lora_target_modules: str = field(default="query,value", metadata={"help": "where to perform LoRA"})
-    # tokenizer_name_or_path: Optional[str] = field(default="zhihan1996/DNABERT-2-117M")
 
 
 @dataclass
@@ -70,9 +61,17 @@ class DataArguments:
 @dataclass
 class TrainingArguments(transformers.TrainingArguments):
     cache_dir: Optional[str] = field(default=None)
+    tokenizer_cache_dir: Optional[str] = field(default=None)
     run_name: str = field(default="run")
     optim: str = field(default="adamw_torch")
     model_max_length: int = field(default=1022, metadata={"help": "Maximum sequence length."})
+    output_dir: str = field(default="output")
+    freeze_backbone: bool = field(default=False)
+    eval_and_save_results: bool = field(default=True)
+    save_model: bool = field(default=True)
+    seed: int = field(default=42)
+    model_type: str = field(default='esm-1b')
+    token_type: str = field(default='esm-1b')
     gradient_accumulation_steps: int = field(default=1)
     per_device_train_batch_size: int = field(default=4)
     per_device_eval_batch_size: int = field(default=4)
@@ -88,27 +87,24 @@ class TrainingArguments(transformers.TrainingArguments):
     save_total_limit: int = field(default=1)
     lr_scheduler_type: str = field(default="cosine_with_restarts")
     load_best_model_at_end: bool = field(default=True)
-    output_dir: str = field(default="output")
     find_unused_parameters: bool = field(default=False)
+    dataloader_num_workers: int = field(default=8)
     checkpointing: bool = field(default=False)
     dataloader_pin_memory: bool = field(default=False)
-    eval_and_save_results: bool = field(default=True)
-    save_model: bool = field(default=True)
-    seed: int = field(default=42)
+    
+    log_dir: str = field(default="output")
+    
     report_to: str = field(default="wandb")
     stage: str = field(default='0')
-    model_type: str = field(default='esm-1b')
-    token_type: str = field(default='esm-1b')
+
     train_from_scratch: bool = field(default=False)
-    log_dir: str = field(default="output")
     attn_implementation: str = field(default="eager")
-    freeze_backbone: bool = field(default=False)
+
     seq_type: str = field(default=None)
     trunc_type: str = field(default=None)
-    # lucaone_args: object = field(default=None) # ?
-    # lucaone_model_args: object = field(default=None) # ?
-    dataloader_num_workers: int = field(default=8) # debug using 0
-
+    lucaone_args: object = field(default=None) # ?
+    lucaone_model_args: object = field(default=None) # ?
+    
 
 
 def set_seed(args):
@@ -137,8 +133,8 @@ class SupervisedDataset(Dataset):
 
         
         # data must in the format of [text, label, *]
-        texts = [d[0].upper() for d in data]
-        labels = [d[1] for d in data]
+        texts = [d[1].upper() for d in data]
+        labels = [d[2] for d in data]
         labels = [json.loads(label) for label in labels if isinstance(label, str)]
 
 
@@ -154,13 +150,34 @@ class SupervisedDataset(Dataset):
 
         self.input_ids = texts
         self.labels = labels
-        self.num_labels = 585
+        self.num_labels = 1
 
     def __len__(self):
         return len(self.input_ids)
 
     def __getitem__(self, i) -> Dict[str, torch.Tensor,]:
         return dict(input_ids=self.input_ids[i], labels=self.labels[i])
+
+
+def gene_seq_replace(seq):
+    '''
+    Nucleic acid gene replace: A->1, U/T->2, C->3, G->4, N->5
+    :param seq:
+    :return:
+    '''
+    new_seq = ""
+    for ch in seq:
+        if ch in ["A", "a"]:
+            new_seq += "1"
+        elif ch in ["T", "U", "t", "u"]:
+            new_seq += "2"
+        elif ch in ["C", "c"]:
+            new_seq += "3"
+        elif ch in ["G", "g"]:
+            new_seq += "4"
+        else: # unknown
+            new_seq += "5"
+    return new_seq
 
 
 
@@ -175,8 +192,9 @@ class DataCollatorForSupervisedDataset:
     def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
         input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids" ,"labels"))
         
-        # if self.args.model_type == 'LucaOne':
-        #     input_ids = [gene_seq_replace(input_id) for input_id in input_ids]
+        if self.args.model_type == 'lucaOne':
+            input_ids = [gene_seq_replace(input_id) for input_id in input_ids]
+
         sequences = self.tokenizer(
             input_ids, 
             padding=True,
@@ -184,118 +202,32 @@ class DataCollatorForSupervisedDataset:
             return_tensors="pt",
             max_length=self.args.model_max_length
         )
-        # if self.args.model_type == 'LucaOne':
-        #     sequences['token_type_ids'] = torch.zeros_like(sequences['attention_mask'])
-            # sequences['position_ids'] = None
+        if self.args.model_type == 'lucaOne':
+            sequences['token_type_ids'] = torch.zeros_like(sequences['attention_mask'])
+            sequences['position_ids'] = None
+
         labels = torch.Tensor(labels) # .int()
         sequences['labels'] = labels
         return sequences
 
 
-"""
-Compute metrics used for huggingface trainer.
-"""
 def calculate_metric_with_sklearn(logits: np.ndarray, labels: np.ndarray):
-    logits = torch.from_numpy(logits)
-    labels = torch.from_numpy(labels)
-    pred = dim_zero_cat(logits)
-    target = dim_zero_cat(labels)
-
-    order = pred.argsort(descending=True, dim=1)
-
-
-    target = target.gather(1, order)
-    precision = target.cumsum(1) / torch.ones_like(target).cumsum(1)
-    recall = target.cumsum(1) / (target.sum(1, keepdim=True) + 1e-10)
-
-    is_start = torch.zeros_like(target).bool()
-    is_start[:, 0] = 1
-    is_start = torch.scatter(is_start, 1, order, is_start)
-    
-    order = order + torch.arange(order.shape[0], device=order.device).unsqueeze(1) * order.shape[1]
-    order = order.flatten()
-
-    inv_order = torch.zeros_like(order)
-    inv_order[order] = torch.arange(order.shape[0], device=order.device)
-
-    all_order = pred.flatten().argsort(descending=True)
-    is_start = is_start.flatten()[all_order]
-    all_order = inv_order[all_order]
-
-    precision = precision.flatten()
-    recall = recall.flatten()
-    
-    all_precision = precision[all_order] - \
-                    torch.where(is_start, torch.zeros_like(precision), precision[all_order - 1])
-    all_precision = all_precision.cumsum(0) / is_start.cumsum(0)
-    all_recall = recall[all_order] - \
-                torch.where(is_start, torch.zeros_like(recall), recall[all_order - 1])
-    all_recall = all_recall.cumsum(0) / pred.shape[0]
-    
-    all_f1 = 2 * all_precision * all_recall / (all_precision + all_recall + 1e-10)
-    return { "fmax": all_f1.max().item()}
+    return {
+        "valid_spearman": spearmanr(logits, labels)[0],
+    }
 
 
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
+    if isinstance(logits, tuple): 
+        logits = logits[0]
+    if logits.ndim == 2: 
+        logits = logits.flatten()
     return calculate_metric_with_sklearn(logits, labels)
-
-
-def convert_lmdb_to_csv():
-    data_root = "/home/bingxing2/ailab/group/ai4bio/public/multi-omics/protein/downstream/EC/AF2/normal"
-    train_path = os.path.join(data_root, "train")
-    valid_path = os.path.join(data_root, "valid")
-    test_path = os.path.join(data_root, "test")
-
-    import lmdb
-    env = lmdb.open(train_path)
-    txn = env.begin()
-
-
-    with open("train.csv", "w") as fw:
-        csv_writer = csv.writer(fw)
-
-        csv_writer.writerow(["text","label","name","chain"])
-
-        length = int(txn.get(str("length").encode()).decode())
-        for index in range(length):
-            item = json.loads(txn.get(str(index).encode()).decode())
-            csv_writer.writerow([item["seq"],item["label"],item["name"],item["chain"]])    
-    
-    env = lmdb.open(valid_path)
-    txn = env.begin()
-
-    with open("valid.csv", "w") as fw:
-        csv_writer = csv.writer(fw)
-
-        csv_writer.writerow(["text","label","name","chain"])
-
-        length = int(txn.get(str("length").encode()).decode())
-        for index in range(length):
-            item = json.loads(txn.get(str(index).encode()).decode())
-            csv_writer.writerow([item["seq"],item["label"],item["name"],item["chain"]])    
-   
-    env = lmdb.open(test_path)
-    txn = env.begin()
-
-
-    with open("test.csv", "w") as fw:
-        csv_writer = csv.writer(fw)
-
-        csv_writer.writerow(["text","label","name","chain"])
-
-        length = int(txn.get(str("length").encode()).decode())
-        for index in range(length):
-            item = json.loads(txn.get(str(index).encode()).decode())
-            csv_writer.writerow([item["seq"],item["label"],item["name"],item["chain"]])    
-    
-
-    exit()
 
 
 
 def train():
-
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
     set_seed(training_args)
@@ -303,9 +235,9 @@ def train():
     # load tokenizer
     print(f"token type is : {training_args.token_type}")
     if training_args.token_type in ["esm-1b"]:
-        cache_dir = os.path.join(training_args.cache_dir, "esm1b_t33_650M_UR50S")
+        cache_dir = os.path.join(training_args.tokenizer_cache_dir, model_args.tokenizer_name_or_path.split("/")[-1])
         tokenizer = EsmTokenizer.from_pretrained(
-            model_args.model_name_or_path,
+            model_args.tokenizer_name_or_path,
             cache_dir=cache_dir,
             model_max_length=training_args.model_max_length,
             padding_side="right",
@@ -328,15 +260,14 @@ def train():
 
 
     # load model
-    cache_dir = os.path.join(training_args.cache_dir, model_args.model_name_or_path.split("/")[-1])
+    model_name_or_path = training_args.cache_dir
     print(f"train_dataset.num_labels: {train_dataset.num_labels}")
     print(f"model type is : {training_args.model_type}")
-    print(f'Loading pretrained ckpts from {cache_dir}')
+    print(f'Loading pretrained ckpts from {model_name_or_path}')
     
-    if training_args.model_type in ["esm-1b", "esm-2_150M"]:      
+    if training_args.model_type in ["esm-1b", "esm-2"]:      
         model = EsmForSequenceClassification.from_pretrained(
-            model_args.model_name_or_path,
-            cache_dir=cache_dir,
+            model_name_or_path,
             num_labels=train_dataset.num_labels,
             ignore_mismatched_sizes=True, 
             pad_token_id=tokenizer.pad_token_id,
@@ -344,12 +275,12 @@ def train():
         )
     elif training_args.model_type == "dnabert2":
         model = DNABERT2ForClassification.from_pretrained(
-            model_args.model_name_or_path,
-            cache_dir=cache_dir,
+            model_name_or_path,
             num_labels=train_dataset.num_labels,
             ignore_mismatched_sizes=True, 
             pad_token_id=tokenizer.pad_token_id,
             vocab_size=tokenizer.vocab_size,
+            use_alibi=model_args.use_alibi
         )
     elif training_args.model_type == "ntv2":
         model = NTv2ForSequenceClassification.from_pretrained(
@@ -362,40 +293,39 @@ def train():
         )
     elif training_args.model_type in ["rna-fm", "beacon"]:
         model = RnaFmForSequenceClassification.from_pretrained(
-            model_args.model_name_or_path,
-            cache_dir=cache_dir,
+            model_name_or_path,
             num_labels=train_dataset.num_labels,
             ignore_mismatched_sizes=True, 
             pad_token_id=tokenizer.pad_token_id,
             vocab_size=tokenizer.vocab_size,
         )
-    elif training_args.model_type in ["rna-fm", "beacon"]:
-        model = RnaFmForSequenceClassification.from_pretrained(
-            model_args.model_name_or_path,
-            cache_dir=cache_dir,
+    elif training_args.model_type in ["lucaone"]:
+        model = LucaOneForSequenceClassification.from_pretrained(
+            model_name_or_path,
             num_labels=train_dataset.num_labels,
             ignore_mismatched_sizes=True, 
             pad_token_id=tokenizer.pad_token_id,
             vocab_size=tokenizer.vocab_size,
         )
     else:
-        assert f"Model Type {training_args.model_type} Is Not Supported."
-
+        raise f"Model Type {training_args.model_type} Is Not Supported."
 
 
     if training_args.freeze_backbone:
-        if training_args.model_type in ['esm2']:
-            no_optim = ["encoder"]
-        elif training_args.model_type in ['esm-1b']:
+        if training_args.model_type in ['esm-2', "esm-1b"]:
+            no_optim = ["embeddings", "encoder"]
+        elif training_args.model_type in ['dnabert2', "ntv2", "rna-fm", "beacon"]:
             no_optim = ["encoder", "embeddings.position_embeddings.weight"]
-        elif training_args.model_type in ["LucaOne"]:
+        elif training_args.model_type in ["lucaOne"]:
             no_optim = ["lucaone"]
         else:
-            no_optim = ["embeddings", "encoder"]
-        for n,p in model.named_parameters():
+            raise "Wrong Model Type"
+
+        for n, p in model.named_parameters():
             if any(nd in n for nd in no_optim):
                 p.requires_grad = False
-        for n,p in model.named_parameters():
+        
+        for n, p in model.named_parameters():
             if p.requires_grad:
                 print(f"gradient parameter: {n}")
 
@@ -410,17 +340,16 @@ def train():
                                    data_collator=data_collator,
                                    callbacks=[early_stopping],
                                    )
-    exit()
+
     trainer.train()
 
-    
     if training_args.save_model:
         trainer.save_state()
-    data_test_list = data_args.data_test_path.replace(" ", "").split(",")
-    print(f"data_test_list = {len(data_test_list)}")
-
-
+    
     if training_args.eval_and_save_results:
+        data_test_list = data_args.data_test_path.replace(" ", "").split(",")
+        print(f"data_test_list = {len(data_test_list)}")
+
         for data_test_name in data_test_list:
             print(f"evaluating data_test_name = {data_test_name}")
             test_dataset = SupervisedDataset(tokenizer=tokenizer, args=training_args,
@@ -431,7 +360,7 @@ def train():
             results_test = trainer.evaluate(eval_dataset=test_dataset)
             with open(os.path.join(results_path, f"{data_test_name}_results.json"), "w") as f:
                 json.dump(results_test, f, indent=4)
-    
+
 
 
 
